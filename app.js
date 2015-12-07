5ab4e7bef3b541d183b9ee338384d40262097031
Asa Smith <smith.asa.la@gmail.com>
2015-12-06 20:40:44 -0500

    Login post handler, login template

--- app.js
+++ app.js
@@ -23,42 +23,60 @@ app.set('view engine', 'jade');
 app.use('/bower_components',
   express.static(__dirname + '/bower_components'));
 
-app.use(bodyParser.urlencoded({ extended: true}));
+app.use(bodyParser.urlencoded({ extended: false}));
+
 app.get('/', function(req, res){
-  res.render('register.jade');
+  res.render('index.jade');
 });
 
 app.get('/register', function(req, res){
+  res.render('register.jade');
+});
+
+app.post('/register', function(req, res){
   var user = new User({
-    firstName: req.body.firstName,
-    lastName: req.body.lastName,
-    userName: req.body.userName,
-    email: req.body.email,
-    password: req.body.password
-  });
-  user.save(function(err){
-    if(err) {
-      var err = 'Nope. Try again';
-      if(err.code === 11000){
-        error = 'A User name is already associated with that email';
-      }
-      res.render('register.jade', {error: error});
-    }else {
-      res.redirect('/dashboard.jade');
+  firstName: req.body.firstName,
+  lastName: req.body.lastName,
+  userName: req.body.userName,
+  email: req.body.email,
+  password: req.body.password
+});
+user.save(function(err){
+  if(err) {
+    var err = 'Nope. Try again';
+    if(err.code === 11000){
+    var  err = 'A User name is already associated with that email';
+    }
+    res.render('register.jade', {error: err});
+  }else {
+    res.redirect('/dashboard');
     }
   });
 });
 
-app.post('/register', function(req, res){
-  res.json(req.body);
-});
 
 app.get('/login', function(req, res){
   res.render('login.jade');
 });
 
+app.post('/login', function(req, res){
+  User.findOne({userName: req.body.userName}, function(err, user){
+    if(!user){
+      res.render('login.jade', {error: 'Nope'});
+    }else{
+      if(req.body.password === user.password){
+        res.redirect('/dashboard');
+      }else{
+        res.render('login.jade', {error: 'Wrong'});
+      }
+    }
+  })
+})
+
 app.get('/dashboard', function(req, res){
   res.render('dashboard.jade');
 });
 
-app.listen(3000);
+app.listen(3000, function(){
+  console.log('Listening...')
+});
